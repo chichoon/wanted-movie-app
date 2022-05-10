274e5cc55f1ddef0024a551c517f216d4bf9720d
chichoon <chichoon.choi@gmail.com>
2022-05-10 21:53:27 +0900

    Feat: axios 부분 별도 함수로 분리

---
+++ src/services/getMovieData.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const getMovieData = (searchValue: string, page: number) => {
+  axios({
+    method: 'GET',
+    params: {
+      apikey: process.env.REACT_APP_API_ID,
+      s: searchValue,
+      page,
+    },
+    url: process.env.REACT_APP_API_URL,
+  });
+};
