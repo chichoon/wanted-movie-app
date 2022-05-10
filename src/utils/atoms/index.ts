a3cc38a2625427da588cc20bf8801f216b001824
chichoon <chichoon.choi@gmail.com>
2022-05-10 18:25:14 +0900

    Feat: Loading 전역상태 설정

---
+++ src/utils/atoms/index.ts
@@ -0,0 +1,5 @@
+import { favoriteDataState } from './favoriteDataState';
+import { searchLoadingState } from './searchLoadingState';
+import { searchValueState } from './searchValueState';
+
+export { favoriteDataState, searchLoadingState, searchValueState };
