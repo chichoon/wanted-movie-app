03af33bc9ad2a584cead602df082153f28e8ba95
chichoon <chichoon.choi@gmail.com>
2022-05-10 22:00:31 +0900

    Feat: env 변수명 수정

--- src/services/getMovieData.ts
+++ src/services/getMovieData.ts
@@ -4,9 +4,9 @@ export const getMovieData = (searchValue: string, page: number) =>
   axios({
     method: 'GET',
     params: {
-      apikey: process.env.REACT_APP_API_ID,
+      apikey: process.env.REACT_APP_MOVIE_API_ID,
       s: searchValue,
       page,
     },
-    url: process.env.REACT_APP_API_URL,
+    url: process.env.REACT_APP_MOVIE_API_URL,
   });
