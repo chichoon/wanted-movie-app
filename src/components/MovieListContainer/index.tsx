22971e5c3d26df52e16e6c9076dfdf4afdcbe47f
chichoon <chichoon.choi@gmail.com>
2022-05-10 17:32:44 +0900

    Feat: custom Hook + recoil 사용해서 데이터 받아오기

--- src/components/MovieListContainer/index.tsx
+++ src/components/MovieListContainer/index.tsx
@@ -1,5 +1,15 @@
+import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { searchValueState } from '../../utils/atoms/searchValueState';
+import { useFetchMovie } from '../../utils/hooks/useFetchMovie';
+
 import styles from './MovieListContainer.module.scss';
 
 export const MovieListContainer = (): JSX.Element => {
-  return <div className={styles.movieContainer}>asdasdsa</div>;
+  const [searchValue, setSearchValue] = useRecoilState(searchValueState);
+  const [searchPage, setSearchPage] = useState(1);
+  const { searchResult, loading } = useFetchMovie(searchPage);
+
+  return <div className={styles.movieContainer}>{searchResult}</div>;
 };
--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { searchValueState } from '../../utils/atoms/searchValueState';
+
 import { SearchIcon } from '../../assets/svgs';
 import styles from './SearchInput.module.scss';
 
 export const SearchInput = (): JSX.Element => {
-  const [searchValue, setSearchValue] = useState('');
+  const [inputValue, setinputValue] = useState('');
+  const [searchValue, setSearchValue] = useRecoilState(searchValueState);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchValue(e.currentTarget.value);
+    setinputValue(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearchValue(inputValue);
   };
 
   return (
     <div className={styles.searchInput}>
-      <form className={styles.searchForm}>
+      <form className={styles.searchForm} onSubmit={handleSubmit}>
         <SearchIcon className={styles.searchIcon} />
         <input type='text' value={searchValue} placeholder='영화 제목을 입력하세요' onChange={handleInputChange} />
         <button type='submit'>검색</button>
