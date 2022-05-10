575bf04d8845754a194eb5e3bd160bbbe125b7c7
chichoon <chichoon.choi@gmail.com>
2022-05-10 17:50:03 +0900

    Feat: MovieBlock props validation 위한 interface 선언

--- src/components/MovieBlock/index.tsx
+++ src/components/MovieBlock/index.tsx
@@ -1,7 +1,11 @@
 import { IMovie } from '../../utils/Interfaces';
 import styles from './MovieBlock.module.scss';
 
-export const MovieBlock = (movieData: IMovie): JSX.Element => {
+interface IMovieBlock {
+  movieData: IMovie;
+}
+
+export const MovieBlock = ({ movieData }: IMovieBlock): JSX.Element => {
   const { Title, Year, imdbID, Type, Poster } = movieData;
   return <li className={styles.movieBlock}>{Title}</li>;
 };
