49779e003882d8caa1dc53d4b2c2ad2cb4616e41
chichoon <chichoon.choi@gmail.com>
2022-05-12 01:20:12 +0900

    feat: getMovieData에서 데이터 받아오도록 수정

--- src/services/getMovieData.ts
+++ src/services/getMovieData.ts
@@ -1,12 +1,20 @@
 import axios from 'axios';
 
-export const getMovieData = (searchValue: string, page: number) =>
-  axios({
-    method: 'GET',
-    params: {
-      apikey: process.env.REACT_APP_MOVIE_API_ID,
-      s: searchValue,
-      page,
-    },
-    url: process.env.REACT_APP_MOVIE_API_URL,
-  });
+export const getMovieData = async (searchValue: string, page: number) => {
+  let response;
+  try {
+    response = await axios({
+      method: 'GET',
+      params: {
+        apikey: process.env.REACT_APP_MOVIE_API_ID,
+        s: searchValue,
+        page,
+      },
+      url: process.env.REACT_APP_MOVIE_API_URL,
+    });
+  } catch (e) {
+    console.log(e);
+    response = null;
+  }
+  return response?.data;
+};
