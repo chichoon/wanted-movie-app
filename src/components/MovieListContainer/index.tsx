45348410ebd78aa4654340d8c3ee22cf300bf4ba
chichoon <chichoon.choi@gmail.com>
2022-05-11 17:20:12 +0900

    Feat: 에러 메시지 정상적으로 출력되도록 수정

--- src/components/MovieListContainer/index.tsx
+++ src/components/MovieListContainer/index.tsx
@@ -4,30 +4,30 @@ import { useRecoilValue } from 'recoil';
 import { searchLoadingState } from '../../utils/atoms';
 import { useFetchMovie } from '../../hooks/useFetchMovie';
 import { MovieBlock } from '../MovieBlock';
-import { LoadingComponent } from './LoadingComponent';
+import { LoadingMessage } from './LoadingMessage';
 
 import styles from './movieListContainer.module.scss';
+import { ErrorMessage } from './ErrorMessage';
 
 export const MovieListContainer = (): JSX.Element => {
   const [searchPage, setSearchPage] = useState(1);
   const loading = useRecoilValue(searchLoadingState);
   const searchResult = useFetchMovie(searchPage);
 
-  console.log(searchResult);
-  return (
-    <div className={styles.movieContainer}>
-      {loading ? (
-        <LoadingComponent />
-      ) : (
-        <ul className={styles.movieList}>
-          {searchResult?.Search.map((v, i) => {
-            const key = `movie-data-#${i}`;
-            return <MovieBlock key={key} movieData={v} />;
-          })}
-        </ul>
-      )}
-    </div>
-  );
+  const renderMovieContainer = (): JSX.Element | null => {
+    if (loading) return <LoadingMessage />;
+    if (searchResult?.Response === 'False') return <ErrorMessage errorString={searchResult?.Error} />;
+    return (
+      <ul className={styles.movieList}>
+        {searchResult?.Search?.map((v, i) => {
+          const key = `movie-data-#${i}`;
+          return <MovieBlock key={key} movieData={v} />;
+        })}
+      </ul>
+    );
+  };
+
+  return <div className={styles.movieContainer}>{renderMovieContainer()}</div>;
 };
 // TODO: Too many results (검색어: a) 예외처리
 // TODO: Movies not found (검색어: harry porter) 예외처리
