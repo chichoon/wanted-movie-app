2b5b22938185f25df2d3085ff063ef7cea46d77b
chichoon <chichoon.choi@gmail.com>
2022-05-12 22:13:43 +0900

    Feat: 별 svg 추가, svg 의미없는 스트로크 삭제

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,4 @@ export { ReactComponent as SearchIcon } from './searchIcon.svg';
 export { ReactComponent as SadIcon } from './sadIcon.svg';
 export { ReactComponent as LoadingIcon } from './loadingIcon.svg';
 export { ReactComponent as ImageNotFoundIcon } from './imageNotFoundIcon.svg';
+export { ReactComponent as StarIcon } from './starIcon.svg';
