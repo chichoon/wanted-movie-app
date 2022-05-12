d27be75d5751d05b8b02c9a6a75757f80e107ec6
chichoon <chichoon.choi@gmail.com>
2022-05-12 13:55:24 +0900

    Feat: 이미지 없음 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,3 +1,4 @@
 export { ReactComponent as SearchIcon } from './searchIcon.svg';
 export { ReactComponent as SadIcon } from './sadIcon.svg';
 export { ReactComponent as LoadingIcon } from './loadingIcon.svg';
+export { ReactComponent as ImageNotFoundIcon } from './imageNotFoundIcon.svg';
