14163f0a2c8beb2dec4344ad24a10ef143ff544e
chichoon <chichoon.choi@gmail.com>
2022-05-13 23:48:40 +0900

    Feat: 페이지 새로고침 시 좋아요 날아가는 현상 수정

--- src/pages/FavoritePage/FavoriteListContainer/FavoriteList.tsx
+++ src/pages/FavoritePage/FavoriteListContainer/FavoriteList.tsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useMount } from 'react-use';
 import { useRecoilState } from 'recoil';
 
-import { IMovie, IFavoriteData } from 'types/movies';
+import { IMovie } from 'types/movies';
 import { favoriteDataState } from 'utils/atoms';
 
 const store = require('store');
@@ -13,14 +13,8 @@ export const FavoriteList = () => {
 
   useMount(() => {
     const storageData = store.get('storageData');
-    if (!storageData) store.set('storageData', favoriteData);
-    else setFavoriteData(storageData);
+    setFavoriteData(storageData);
   });
-  // TODO: 페이지 새로고침 시에 로컬스토리지 데이터 다 날아가는거 다시 보기
-
-  useEffect(() => {
-    store.set('storageData', favoriteData);
-  }, [favoriteData]);
 
   return (
     <ul>
