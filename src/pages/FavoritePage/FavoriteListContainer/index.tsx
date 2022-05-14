d22153b3669b0c890affb1efba63729462cedb2f
chichoon <chichoon.choi@gmail.com>
2022-05-15 00:41:18 +0900

    Feat: Drag and Drop 으로 순서변경 구현

--- src/pages/FavoritePage/FavoriteListContainer/index.tsx
+++ src/pages/FavoritePage/FavoriteListContainer/index.tsx
@@ -1,4 +1,6 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import _ from 'lodash';
 
 import { MovieBlock } from 'components';
 import { IMovie } from 'types/movies';
@@ -7,17 +9,47 @@ import { favoriteDataState } from 'utils/atoms';
 import styles from './favoriteListContainer.module.scss';
 
 export const FavoriteListContainer = () => {
-  const favoriteData = useRecoilValue(favoriteDataState);
+  const [favoriteData, setFavoriteData] = useRecoilState(favoriteDataState);
+
+  const handleDragEnd = (r: DropResult) => {
+    const movieArrTemp = _.cloneDeep(favoriteData.Movies);
+    const imdbArrTemp = _.cloneDeep(favoriteData.imdbIDs);
+
+    const movieArrSrc = movieArrTemp.splice(r.source.index, 1)[0];
+    const imdbArrSrc = imdbArrTemp.splice(r.source.index, 1)[0];
+    movieArrTemp.splice(r.destination?.index ?? 0, 0, movieArrSrc);
+    imdbArrTemp.splice(r.destination?.index ?? 0, 0, imdbArrSrc);
+
+    setFavoriteData({
+      Movies: movieArrTemp,
+      imdbIDs: imdbArrTemp,
+    });
+  };
 
   return (
     <div className={styles.favoriteContainer}>
-      <ul>
-        {favoriteData.Movies.map((v: IMovie) => (
-          <li key={`fav-list-${v.imdbID}`}>
-            <MovieBlock movieData={v} />
-          </li>
-        ))}
-      </ul>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId='favoriteData'>
+          {(droppableProv) => (
+            <ul className={styles.favoriteUL} {...droppableProv.droppableProps} ref={droppableProv.innerRef}>
+              {favoriteData.Movies.map((v: IMovie, i: number) => (
+                <Draggable key={`fav-list-${v.imdbID}`} draggableId={`fav-list-${v.imdbID}`} index={i}>
+                  {(DraggableProv) => (
+                    <li
+                      ref={DraggableProv.innerRef}
+                      {...DraggableProv.dragHandleProps}
+                      {...DraggableProv.draggableProps}
+                    >
+                      <MovieBlock movieData={v} />
+                    </li>
+                  )}
+                </Draggable>
+              ))}
+              {droppableProv.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
